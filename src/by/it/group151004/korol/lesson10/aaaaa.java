b4a60d817db916667a5276c541512e8d2c15ea25
korol <korol634921777@gmail.com>
2022-12-23 00:33:06 +0300

    Added lessons 9 10 (A B)

---
+++ src/by/it/group151004/korol/lesson10/aaaaa.java
@@ -0,0 +1,309 @@
+package by.it.group151004.korol.lesson10;
+
+import java.util.*;
+
+public class aaaaa<E extends Comparable<E>>  implements NavigableSet<E> {
+
+    private class Node<T extends Comparable<T>> {
+        public T value;
+        public Node nodeR;
+        public Node nodeL;
+
+        public Node() {
+
+        }
+    }
+    //Создайте БЕЗ использования других классов (включая абстрактные)
+    //аналог дерева TreeSet
+    private Node m_node;
+    private int m_size;
+
+    //Обязательные к реализации методы и конструкторы
+    public aaaaa() { m_node = null; m_size = 0;}
+
+    @Override
+    public boolean add(E e) {
+        if (e == null)
+            return false;
+        Node node = new Node();
+        node.value = e;
+        node.nodeL = null;
+        node.nodeR = null;
+        if(m_node == null){
+            m_node = node;
+            ++m_size;
+        } else {
+            Node current = m_node;
+            Node prev = null;
+            while (true){
+                if (current.value.equals(e))
+                    return false;
+                prev = current;
+                if(e.compareTo((E) prev.value) < 0){
+                    current = current.nodeL;
+                    if(current==null){
+                        prev.nodeL = node;
+                        ++m_size;
+                        return true;
+                    }
+                }else{
+                    current = current.nodeR;
+                    if(current==null){
+                        prev.nodeR = node;
+                        ++m_size;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private Node getMinimumKey(Node node) {
+        while (node.nodeL != null) {
+            node = node.nodeL;
+        }
+        return node;
+    }
+
+    private boolean delete(Node node, E value) {
+        Node parent = null;
+        Node current = m_node;
+
+        //Find node, which contains value, we have to remove
+        while (current != null && !current.value.equals(value))
+        {
+            parent = current;
+            if (value.compareTo((E) current.value) < 0) {
+                current = current.nodeL;
+            }
+            else {
+                current = current.nodeR;
+            }
+        }
+
+        //Return false if there is no such value in tree
+        if (current == null) {
+            return false;
+        }
+
+        //If node is leaf
+        if (current.nodeL == null && current.nodeR == null) {
+            //If this leaf isn't tree root
+            if (!current.equals(m_node))
+            {
+                if (parent.nodeL == current) {
+                    parent.nodeL = null;
+                }
+                else {
+                    parent.nodeR = null;
+                }
+            }
+
+            //If it's root we can just make it null
+            else {
+                m_node = null;
+            }
+        }
+
+        //Deleted node has 2 offsprings
+        else if (current.nodeL != null && current.nodeR != null) {
+            //Find the most left subtree element of right offspring
+            Node successor = getMinimumKey(current.nodeR);
+
+            //Remember value of that element
+            E val = (E) successor.value;
+
+            //Delete the most left element of right offspring(notice: it obviously has less than 2 offsprings)
+            delete(m_node, (E)successor.value);
+
+            //Copy value to our node
+            current.value = val;
+        }
+
+        //Deleted node has 1 offspring
+        else {
+            Node child = (current.nodeL != null) ? current.nodeL : current.nodeR;
+
+            //If deleted node isn't tree root
+            if (!current.equals(m_node))
+            {
+                if (current == parent.nodeL) {
+                    parent.nodeL = child;
+                }
+                else {
+                    parent.nodeR = child;
+                }
+            }
+
+            //If deleted node is root we can just assign it to child
+            else {
+                m_node = child;
+            }
+        }
+        return true; //Case where we can't delete node and return false is above
+    }
+
+    @Override
+    public boolean remove(Object o) { --m_size; return delete(m_node, (E) o); } //Calls recursive function
+
+    private String str(Node node, StringJoiner txt) {
+        if (node == null)
+            return txt.toString();
+        str(node.nodeL, txt);
+        txt.add(node.value.toString());
+        str(node.nodeR, txt);
+        return txt.toString();
+    }
+
+    @Override
+    public String toString() { //Calls recursive function
+        StringJoiner txt = new StringJoiner(", ", "[", "]");;
+        return str(m_node, txt);
+    }
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    ////////         Эти методы реализовывать необязательно      ////////////
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public E lower(E e) {
+        return null;
+    }
+
+    @Override
+    public E floor(E e) {
+        return null;
+    }
+
+    @Override
+    public E ceiling(E e) {
+        return null;
+    }
+
+    @Override
+    public E higher(E e) {
+        return null;
+    }
+
+    @Override
+    public E pollFirst() {
+        return null;
+    }
+
+    @Override
+    public E pollLast() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public NavigableSet<E> descendingSet() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> headSet(E toElement, boolean inclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
+        return null;
+    }
+
+    @Override
+    public Comparator<? super E> comparator() {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> subSet(E fromElement, E toElement) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> headSet(E toElement) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> tailSet(E fromElement) {
+        return null;
+    }
+
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E last() {
+        return null;
+    }
+}
---
+++ src/by/it/group151004/korol/lesson9/ListB.java
@@ -0,0 +1,179 @@
+package by.it.group151004.korol.lesson9;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListB<T> implements List<T>{
+
+    public Object[] arr = new Object[100];
+    public int amount = 0;
+
+    @Override
+    public boolean add(T t) {
+        if (amount == arr.length - 1) {
+            Object[] newArray = new Object[arr.length * 2];
+            System.arraycopy(arr, 0, newArray, 0, amount);
+            arr = newArray;
+        }
+        arr[amount++] = t;
+        return true;
+    }
+
+    @Override
+    public T remove(int index) {
+        T res = (T) arr[index];
+        for (int i = index; i < amount; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[amount] = null;
+        amount--;
+        return res;
+    }
+
+    @Override
+    public T get(int index) {
+        return (T) arr[index];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("[");
+        for (int i = 0; i < amount; i++) {
+            str.append(arr[i]);
+            str.append(", ");
+        }
+        str.delete(str.length() - 2, str.length());
+        str.append("]");
+
+        return str.toString();
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (amount == arr.length - 1) {
+            Object[] newA = new Object[arr.length * 2];
+            System.arraycopy(arr, 0, newA, 0, amount);
+            arr = newA;
+        }
+        for (int i = amount; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = element;
+        amount++;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        T res = (T) arr[index];
+        arr[index]=element;
+        return res;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        Object[] newArray = new Object[amount + c.size()];
+        System.arraycopy(arr, 0, newArray, 0, amount);
+        arr = newArray;
+        for (Object el : c) {
+            arr[amount++] = el;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i=0;i<amount;i++){
+            if(arr[i]==o){return true;}
+        }
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i=0;i<amount;i++){
+            if(arr[i]==o){return i;}
+        }
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return amount;
+    }
+
+    @Override
+    public boolean isEmpty() {
+
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
+
